e16e0d404103e787f74aaf4924e283968815093e
Akratash <patrickbuehler@hotmail.de>
2019-04-13 11:44:49 +0200

    Add SetStone

--- ch.akratash.viergewinnt.model/src/ch/akratash/viergewinnt/model/Board.java
+++ ch.akratash.viergewinnt.model/src/ch/akratash/viergewinnt/model/Board.java
@@ -4,10 +4,12 @@ public class Board {
 
 	private Color[][] m_grid;
 	private boolean m_gameOver;
+	private Color m_activePlayer;
 
 	public Board() {
 		m_gameOver = false;
 		m_grid = new Color[7][6];
+		m_activePlayer = Color.RED;
 
 		for (Color[] column : m_grid) {
 			for (Color field : column) {
@@ -17,4 +19,33 @@ public class Board {
 
 	}
 
+	private void switchPlayer() {
+		if (m_gameOver) {
+			m_activePlayer = Color.NONE;
+		} else if (m_activePlayer == Color.RED) {
+			m_activePlayer = Color.YELLOW;
+		} else if (m_activePlayer == Color.YELLOW) {
+			m_activePlayer = Color.RED;
+		}
+	}
+
+	public boolean setStone(int column) {
+		boolean result = false;
+
+		if (column >= 0 && column < m_grid.length) {
+
+			int rowIndex = 0;
+			while (m_grid[column][rowIndex] != Color.NONE) {
+				rowIndex++;
+			}
+
+			if (rowIndex < m_grid[column].length) {
+				m_grid[column][rowIndex] = m_activePlayer;
+				switchPlayer();
+				result = true;
+			}
+		}
+
+		return result;
+	}
 }
